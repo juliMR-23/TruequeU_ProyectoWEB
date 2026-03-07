6aa4a64740c42399b887b8f3686329c60fc7a51f
Julián MR <julian.marin42@eia.edu.co>
2026-03-06 21:18:44 -0500

    Asset/logo.svg mejor formato para imágenes web, layout inicial, B&W

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,20 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
-      <main>
-        <p>Un lugar para compartir</p>
-        <h1>Bienvenido!</h1>
-      </main>
+      <div className="flex flex-col items-center">
+        <main className="text-3xl text-center m-10">
+          <p>Un lugar para compartir</p>
+          <h1 className="font-bold text-6xl">Bienvenid@!</h1>
+        </main>
+        <div className="flex flex-row items-center gap-6">
+          <button className="bg-black text-white font-semibold tracking-wide p-2 rounded-lg">
+            Registrarse
+          </button>
+          <button className="bg-black text-white font-semibold tracking-wide p-2 rounded-lg">
+            Iniciar sesión
+          </button>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,15 +1,16 @@
+import logo from '../../assets/logo.svg';
+
 export default function Navbar (){
     return(
-        <header className="w-full bg-white flex items-center justify-between px-6 py-3">
+        <header className="w-full bg-black flex items-center justify-between px-6 py-3 text-white">
             <div className="flex items-center gap-3">
-                <img className="w-20 h-20" src="https://media.istockphoto.com/id/2159032510/es/vector/icono-de-interruptor-s%C3%ADmbolo-de-flechas-de-sincronizaci%C3%B3n-ilustraci%C3%B3n-vectorial-de.jpg?s=612x612&w=0&k=20&c=hHU8HpgFlVvhZYMrXLLuM7cf8MYYSlEliJjRbzPNskM=" alt="logo"/>
-                <h2 className="text-black text-3xl font-bold tracking-widest">TRUEQUE U</h2>
+                <img className="w-10 h-10" src={logo} alt="logo"/>
+                <h2 className="text-3xl font-bold tracking-widest">TRUEQUE U</h2>
             </div>
-            
             <nav className="flex gap-6">
-                <a href="#" onClick={(e)=>e.preventDefault()}>Home</a>
-                <a href="#" onClick={(e)=>e.preventDefault()}>Publishing</a>
-                <a href="#" onClick={(e)=>e.preventDefault()}>Chat</a>
+                <a className="hover:underline" href="#" onClick={(e)=>e.preventDefault()}>Home</a>
+                <a className="hover:underline" href="#" onClick={(e)=>e.preventDefault()}>Publishing</a>
+                <a className="hover:underline" href="#" onClick={(e)=>e.preventDefault()}>Chat</a>
             </nav>
         </header>
     );
