b26be58eef888c783f9c8d1044773748755bad22
Maria José Sosa <maria.sosa15@eia.edu.co>
2026-03-13 23:56:56 -0500

    adding images to the listings

--- src/components/listings/ListingCard.tsx
+++ src/components/listings/ListingCard.tsx
@@ -13,49 +13,62 @@ type Props = {
 export default function ListingCard({ listing }: Props) {
     const isSold: boolean = listing.status === ListingStatusEnum.sold;
     const { isFavorite, toggle } = useFavorites();
+    // toma la primera imagen de la clase Listing, para poderla mostrar
+    const coverImage = listing.images.find(img => img.order === 0) ?? listing.images[0];
 
     return (
         <article className="rounded-card border border-border bg-surface p-4 shadow-card rounded-2xl flex flex-col">
+            <div className="relative mb-4">
+                <img
+                    src={coverImage.url}
+                    alt={listing.title}
+                    className="w-full h-48 object-cover rounded-2xl"
+                />
+                <div className="absolute top-2 right-2">
+                    <Badge variant={isSold ? "danger" : "success"}>
+                        {isSold ? ListingStatusEnum.sold : ListingStatusEnum.available}
+                    </Badge>
+                </div>
+            </div>
+
             <div className="flex items-start justify-between gap-3">
                 <div>
                     {/* Titulo */}
-                    <h3 className="m-0 text-base font-semibold text-text">{listing.title}</h3>
+                    <h3 className="m-0 text-base font-bold text-text">{listing.title}</h3>
                     {/* Categoria */}
                     <span className="mt-1 text-xs text-muted">{listing.category}</span>
                     {/* Descripción */}
                     <p className="mt-1 text-xs text-muted">{listing.description}</p>
                 </div>
-                {/* Muestra el estado: "SOLD_OUT" si está agotado, "AVAILABLE" si hay entradas */}
-                <Badge variant={isSold ? "danger" : "success"}>
-                    <div>
-                        {isSold ? ListingStatusEnum.sold : ListingStatusEnum.available}
-                    </div>
-                </Badge>
             </div>
             <div className="mt-3 space-y-2 text-sm text-text">
                 <p className="m-0">
+                    {/* Condición */}
                     <span className="font-bold">Condition:</span>{" "}
                     <span className="text-muted">{listing.condition}</span>
                 </p>
                 <p className="m-0">
+                    {/* Ubicación */}
                     <span className="font-bold">Location:</span>{" "}
                     <span className="text-muted">
                         {listing.location}
                     </span>
                 </p>
             </div>
-
+            {/* Ir a detalles */}
             <div className="my-3 rounded-btn  bg-surface px-1 py-2 text-sm font-medium shadow-card hover:bg-gray-50">
                 <Link to={`/listings/${listing.id}`} className="text-eia-azul-claro font-extrabold">
                     Details
                 </Link>
             </div>
             <div className="mt-auto flex items-center justify-between">
+                {/* precio */}
                 <p className="m-0 font-semibold text-text">${listing.price}</p>
                 {/* <Button variant="primary" disabled={isSold} onClick={() => onAddToCart(listing)}>
           <FiShoppingCart />
           {isSold ? "Unavailable" : "Add"}
         </Button> */}
+                {/* Añadir/quitar de favoritos */}
                 <div className="bottom-2 right-2">
                     <FavoriteButton
                         listingId={listing.id}
