c88032a0470bfbe6adeb75ecefcf2109a0189b34
Maria José Sosa <maria.sosa15@eia.edu.co>
2026-03-16 09:32:24 -0500

    Deploy 2.3

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/TruequeU_ProyectoWEB'>
+    <BrowserRouter basename='/TruequeU_ProyectoWEB/'>
       <App />
     </BrowserRouter>
   </StrictMode>,
