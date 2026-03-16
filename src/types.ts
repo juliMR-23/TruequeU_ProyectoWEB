37cafbb38173666c796af4286a06f0db4a8973e6
Julián MR <julian.marin42@eia.edu.co>
2026-03-16 14:01:07 -0500

    Comenzando a trabajar en gestión de usuario y register/login real, añadiendo mock data

--- src/types.ts
+++ src/types.ts
@@ -24,3 +24,13 @@ export interface ListingImage {
     url: string;
     order: number;
 }
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password?: string;//Opcional porque no siempre va a UI
+  major: string;//Carrera del estudiante
+  avatar?: string;// URL (imagen de perfil)
+  createdAt?: string; 
+}
